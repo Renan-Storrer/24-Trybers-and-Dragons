347c0ab3b09b12a0ed9ac6349d01ffd4bbfdfa2b
Renan-Storrer <storrerrenan@gmail.com>
2023-05-06 02:58:06 -0300

    feat:Cria classes que herdam de 'Race' - (req02)

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,22 @@
+import Race from "./Race";
+
+class Dwarf extends Race {
+  public _maxLifePoints: number;
+  private static _instancesNumber = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+    Dwarf._instancesNumber += 1;
+  }
+
+  override get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static override createdRacesInstances(): number {
+    return this._instancesNumber
+  }
+}
+
+export default Dwarf;
