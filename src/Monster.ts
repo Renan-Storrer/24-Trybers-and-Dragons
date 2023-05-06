b00738d17601c87a6c992711ea4e177a0e64205f
Renan-Storrer <storrerrenan@gmail.com>
2023-05-06 18:06:53 -0300

    feat: Cria Classe Monster - (req09)

---
+++ src/Monster.ts
@@ -0,0 +1,25 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  constructor(private _lifePoints = 85, private _strength = 63) {}
+  
+  public get lifePoints() : number {
+    return this._lifePoints;
+  }
+
+  public get strength() : number {
+    return this._strength;
+  }
+  
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+    if (this.lifePoints <= 0) this._lifePoints = -1;
+    return this._lifePoints;
+  }
+}
+
+export default Monster;
