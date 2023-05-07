64219e84dc5327fc6800d37a7ea6771450fa08dd
Renan-Storrer <storrerrenan@gmail.com>
2023-05-06 22:02:22 -0300

    feat: Cria classe PVE - (req11)

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,23 @@
+import Battle from "./Battle";
+import Fighter, { SimpleFighter } from "../Fighter";
+
+class PVE extends Battle {
+  constructor(public character: Fighter, public environment: SimpleFighter[]) {
+    super(character);
+  }
+
+  override fight(): number {
+    while (
+      this.environment.some(({ lifePoints }) => lifePoints > 0)
+    ) {
+      this.environment.forEach((e) => {
+        this.character.attack(e);
+        e.attack(this.character);
+        if (this.character.lifePoints > 0) return -1;
+      });
+    }
+    return super.fight();
+  }
+}
+
+export default PVE;
